fb957b5a3831fdb63b8fa1d4408cfb0fda82cdab
vannseavlong <137812405+LONGCELOT@users.noreply.github.com>
2025-11-07 23:33:29 +0700

    fix: application build

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,14 +3,24 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   // Redirect API calls to backend
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${apiUrl}/api/:path*`,
       },
     ];
   },
